3ebf80591151c2f672949b577f0bfa3f2a84f4e0
Sarobidy-23 <hei.sarobidy@gmail.com>
2023-04-17 16:09:01 +0300

    chore: initial commit

---
+++ src/test/java/com/example/mozart/MozartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mozart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MozartApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
